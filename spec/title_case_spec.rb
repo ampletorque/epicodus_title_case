a785b44f4ef31c240381a1460e3890ebb6f54c0f
andrew david burt & james robert williams <student@epicodus.com>
2015-04-22 10:13:47 -0700

    passed final spec

--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -5,4 +5,7 @@ describe('String#title_case') do
    it("capitalizes first letter of word") do
       expect("beowulf".title_case()).to(eq("Beowulf"))
    end
+   it("capitalizes first letter of multiple word title") do
+     expect("the color purple".title_case()).to(eq("The Color Purple"))
+   end
 end
